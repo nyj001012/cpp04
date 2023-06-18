0c716ac32636cad524793ed7e71df71374c7f7ea
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-18 23:44:50 +0900

    chore: format output

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 23:07:56 by yena              #+#    #+#             */
-/*   Updated: 2023/06/18 23:37:51 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/18 23:43:22 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,24 @@
 #include "WrongCat.hpp"
 
 int main(void) {
+  std::cout << "===================== Constructor =====================" << std::endl;
   const Animal *meta = new Animal();
   const Animal *j = new Dog();
   const Animal *i = new Cat();
   const WrongAnimal *wrong_animal = new WrongCat();
 
+  std::cout << std::endl;
+  std::cout << "======================= GetType =======================" << std::endl;
   std::cout << j->getType() << " " << std::endl;
   std::cout << i->getType() << " " << std::endl;
+  std::cout << std::endl;
+  std::cout << "====================== MakeSound ======================" << std::endl;
   i->makeSound();
   j->makeSound();
   meta->makeSound();
   wrong_animal->makeSound();
-
+  std::cout << std::endl;
+  std::cout << "===================== Destructor ======================" << std::endl;
   delete meta;
   delete j;
   delete i;
