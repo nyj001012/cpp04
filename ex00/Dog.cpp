63f40fa3d784bfc3e565a11daa17e48179bd8e5b
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-18 23:32:31 +0900

    feat: implement dog class

---
+++ ex00/Dog.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/18 23:30:39 by yena              #+#    #+#             */
+/*   Updated: 2023/06/18 23:32:13 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog(void) : Animal() {
+  std::cout << "Dog Constructor called" << std::endl;
+  this->type = "Dog";
+}
+
+Dog::Dog(const Dog &other) {
+  std::cout << "Dog Copy Constructor called" << std::endl;
+  *this = other;
+}
+
+Dog &Dog::operator=(const Dog &other) {
+  std::cout << "Dog Assignation Operator called" << std::endl;
+  this->type = other.type;
+  return (*this);
+}
+
+Dog::~Dog(void) {
+  std::cout << "Dog Destructor called" << std::endl;
+}
+
+/**
+ * @brief Make sound of Dog
+ */
+void Dog::makeSound(void) const {
+  std::cout << "Dog barks" << std::endl;
+}
