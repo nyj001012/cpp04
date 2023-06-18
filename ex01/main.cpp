9c56c4ff1c21e2c13b2a9d9b44ce018c6a83f1d4
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-19 07:03:16 +0900

    feat: fix leak and change name of function

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 23:07:56 by yena              #+#    #+#             */
-/*   Updated: 2023/06/19 00:56:46 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/19 07:02:47 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
  * @details This test is for checking basic constructor, destructor
  */
 void basicTest(void) {
-  std::cout << "\033[0;32m1. Basic Test\033[0m" << std::endl;
+  std::cout << "\033[0;32mBasic Test\033[0m" << std::endl;
   std::cout << "===================== Constructor =====================" << std::endl;
   const Animal *j = new Dog();
   const Animal *i = new Cat();
@@ -36,8 +36,9 @@ void basicTest(void) {
  * @return Animal** farm
  */
 Animal **createFarm(void) {
+  std::cout << "\033[0;32mCreate Test\033[0m" << std::endl;
   std::cout << "===================== Constructor =====================" << std::endl;
-  Animal **farm = new Animal*[10];
+  Animal **farm = new Animal *[10];
   for (int i = 0; i < 5; i++) {
     farm[i] = new Cat();
     Cat *cat = static_cast<Cat *>(farm[i]);
@@ -53,15 +54,26 @@ Animal **createFarm(void) {
 }
 
 /**
- * @brief Make sound test
- * @details This function makes sound of farm
- * @param farm
+ * @brief Make sound and Brain test
+ * @param Animal *farm[10]
  */
-void makeSoundTest(Animal *farm[10]) {
+void functionTest(Animal *farm[10]) {
+  std::cout << "\033[0;32mMakeSound & Brain Test\033[0m" << std::endl;
   std::cout << "====================== MakeSound ======================" << std::endl;
   for (int i = 0; i < 10; i++) {
     farm[i]->makeSound();
   }
+  std::cout << "======================== Brain ========================" << std::endl;
+  Cat *cat = static_cast<Cat *>(farm[0]);
+  Dog *dog = static_cast<Dog *>(farm[5]);
+  std::cout << "A Cat's idea: " << cat->getIdea(0) << std::endl;
+  std::cout << "A Dog's idea: " << dog->getIdea(5) << std::endl;
+  std::cout << std::endl;
+  std::cout << "A Cat and A Dog changed it's idea!\n" << std::endl;
+  cat->setIdea(0, "I need to sleep...");
+  dog->setIdea(5, "Let's go for a walk!!!");
+  std::cout << "A Cat's idea: " << cat->getIdea(0) << std::endl;
+  std::cout << "A Dog's idea: " << dog->getIdea(5) << std::endl;
   std::cout << std::endl;
 }
 
@@ -72,17 +84,19 @@ void makeSoundTest(Animal *farm[10]) {
  */
 
 void destroyFarm(Animal *farm[10]) {
+  std::cout << "\033[0;32mEnd of Test\033[0m" << std::endl;
   std::cout << "===================== Destructor ======================" << std::endl;
   for (int i = 0; i < 10; i++) {
     delete farm[i];
   }
+  delete[] farm;
   std::cout << std::endl;
 }
 
 int main(void) {
   basicTest();
   Animal **farm = createFarm();
-  makeSoundTest(farm);
+  functionTest(farm);
   destroyFarm(farm);
   return (0);
 }
