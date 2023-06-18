21e9dfe53a2fed43eac0d8537d14fb1e05e5838c
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-19 00:01:09 +0900

    feat: implement main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 23:07:56 by yena              #+#    #+#             */
-/*   Updated: 2023/06/18 23:43:22 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/19 00:00:31 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,30 @@
 
 int main(void) {
   std::cout << "===================== Constructor =====================" << std::endl;
-  const Animal *meta = new Animal();
   const Animal *j = new Dog();
   const Animal *i = new Cat();
-  const WrongAnimal *wrong_animal = new WrongCat();
-
-  std::cout << std::endl;
-  std::cout << "======================= GetType =======================" << std::endl;
-  std::cout << j->getType() << " " << std::endl;
-  std::cout << i->getType() << " " << std::endl;
-  std::cout << std::endl;
-  std::cout << "====================== MakeSound ======================" << std::endl;
-  i->makeSound();
-  j->makeSound();
-  meta->makeSound();
-  wrong_animal->makeSound();
   std::cout << std::endl;
   std::cout << "===================== Destructor ======================" << std::endl;
-  delete meta;
   delete j;
   delete i;
-  delete wrong_animal;
+  std::cout << std::endl;
+  std::cout << "===================== Constructor =====================" << std::endl;
+  Animal farm = new Animal[10];
+  for (int i = 0; i < 5; i++) {
+    farm[i] = new Cat();
+    for (int j = 0; j < 100; j++) {
+      farm[i]->setIdea(j, "I want a fish!");
+    }
+  }
+  for (int i = 5; i < 10; i++) {
+    farm[i] = new Dog();
+    for (int j = 0; j < 100; j++) {
+      farm[i]->setIdea(j, "I want a bone!");
+    }
+  }
+  std::cout << std::endl;
+  std::cout << "======================== Brain ========================" << std::endl;
+  std::cout << "Cat's 1st idea of brain: " << farm[0]->getIdea(0) << std::endl;
+  std::cout << "Dog's 1st idea of brain: " << farm[5]->getIdea(0) << std::endl;
   return (0);
 }
