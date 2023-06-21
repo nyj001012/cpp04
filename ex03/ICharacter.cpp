8050bf9494eb2fdc700508b78d8c5e7c77331157
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-21 20:42:43 +0900

    feat: implement ICharacter class

--- ex03/ICharacter.cpp
+++ ex03/ICharacter.cpp
@@ -1,5 +1,94 @@
-//
-// Created by 나예진 on 2023/06/21.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 20:11:04 by yena              #+#    #+#             */
+/*   Updated: 2023/06/21 20:41:48 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ICharacter.hpp"
+
+ICharacter::ICharacter(void) {
+  std::cout << F_YELLOW << "[ ICharacter ]" << FB_DEFAULT
+            << "ICharacter Constructor called" << std::endl;
+}
+
+ICharacter::ICharacter(const ICharacter &other) {
+  std::cout << F_YELLOW << "[ ICharacter ]" << FB_DEFAULT
+            << "ICharacter Copy Constructor called" << std::endl;
+  if (this != &other)
+    *this = other;
+}
+
+ICharacter &ICharacter::operator=(const ICharacter &other) {
+  std::cout << F_YELLOW << "[ ICharacter ]" << FB_DEFAULT
+            << "ICharacter Assignation Operator called" << std::endl;
+  return (*this);
+}
+
+ICharacter::~ICharacter(void) {
+  std::cout << F_YELLOW << "[ ICharacter ]" << FB_DEFAULT
+            << "ICharacter Destructor called" << std::endl;
+}
+
+/**
+ * @brief Get name of ICharacter
+ * @return std::string
+ */
+std::string const &ICharacter::getName(void) const {
+  std::cout << F_YELLOW << "[ ICharacter ]" << FB_DEFAULT
+            << "ICharacter getName called" << std::endl;
+  return (this->_name);
+}
+
+/**
+ * @brief Equip materia
+ * @param materia
+ */
+void ICharacter::equip(AMateria *materia) {
+  std::cout << F_YELLOW << "[ ICharacter ]" << FB_DEFAULT
+            << "ICharacter equip called" << std::endl;
+  for (int i = 0; i < SLOT_MAX; i++) {
+    if (this->_materia[i] == NULL) {
+      this->_materia[i] = materia;
+      return ;
+    }
+  }
+  std::cout << F_RED << "[ ICharacter ]" << FB_DEFAULT
+            << "Your inventory is full!" << std::endl;
+}
+
+/**
+ * @brief Unequip materia
+ * @param idx
+ */
+void ICharacter::unequip(int idx) {
+  if (this->_mataria[idx] == NULL) {
+    std::cout << F_RED << "[ ICharacter ]" << FB_DEFAULT
+              << "The inventory slot is empty!" << std::endl;
+    return ;
+  }
+  std::cout << F_YELLOW << "[ ICharacter ]" << FB_DEFAULT
+            << "ICharacter unequip called" << std::endl;
+  this->_materia[idx] = NULL;
+}
+
+/**
+ * @brief Use materia
+ * @param idx
+ * @param target
+ */
+void ICharacter::use(int idx, ICharacter &target) {
+  if (this->_mataria[idx] == NULL) {
+    std::cout << F_RED << "[ ICharacter ]" << FB_DEFAULT
+              << "You can not use nothing!" << std::endl;
+    return ;
+  }
+  std::cout << F_YELLOW << "[ ICharacter ]" << FB_DEFAULT
+            << "ICharacter use called" << std::endl;
+  this->_materia[idx]->use(target);
+}
--- ex03/ICharacter.hpp
+++ ex03/ICharacter.hpp
@@ -6,13 +6,14 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 20:11:04 by yena              #+#    #+#             */
-/*   Updated: 2023/06/21 20:13:20 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/21 20:34:31 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CPP04_EX03_ICHARACTER_HPP_
 #define CPP04_EX03_ICHARACTER_HPP_
 
+# include "font.hpp"
 # define SLOT_MAX 4
 
 class ICharacter {
@@ -21,8 +22,11 @@ class ICharacter {
   std::string name;
 
  public:
-  virtual ~ICharacter() {}
-  virtual std::string const &getName() const = 0;
+  ICharacter(void);
+  ICharacter(const ICharacter &other);
+  ICharacter &operator=(const ICharacter &other);
+  virtual ~ICharacter(void) {}
+  virtual std::string const &getName(void) const = 0;
   virtual void equip(AMateria *m) = 0;
   virtual void unequip(int idx) = 0;
   virtual void use(int idx, ICharacter &target) = 0;
