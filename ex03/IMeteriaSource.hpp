18c0b4dcb6ffb1b6645fade1d6222b6d934e8a09
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-21 20:42:37 +0900

    feat: implement IMeteriaSource class

--- ex03/IMeteriaSource.hpp
+++ ex03/IMeteriaSource.hpp
@@ -1,23 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   IMeteriaSource.hpp                                 :+:      :+:    :+:   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 20:14:35 by yena              #+#    #+#             */
-/*   Updated: 2023/06/21 20:14:49 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/21 20:25:30 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CPP04_EX03_IMETERIASOURCE_HPP_
-#define CPP04_EX03_IMETERIASOURCE_HPP_
+#ifndef CPP04_EX03_IMateriaSource_HPP_
+#define CPP04_EX03_IMateriaSource_HPP_
 
-class IMeteriaSource {
+# include <iostream>
+# include "AMateria.hpp"
+# include "font.hpp"
+
+class IMateriaSource {
+ private:
+  AMateria *_materia[4];
+
  public:
+  IMateriaSource(void);
+  IMateriaSource(const IMateriaSource &other);
+  IMateriaSource &operator=(const IMateriaSource &other);
+  ~IMateriaSource(void);
   virtual ~IMateriaSource() {}
   virtual void learnMateria(AMateria *) = 0;
   virtual AMateria *createMateria(std::string const &type) = 0;
 };
 
-#endif //CPP04_EX03_IMETERIASOURCE_HPP_
+#endif //CPP04_EX03_IMateriaSource_HPP_
